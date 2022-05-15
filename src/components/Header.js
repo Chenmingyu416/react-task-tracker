2741c67e685da6456c1b11a11894d592f65ce1ce
mingyuchen <chen_mingyu416@sina.com>
2022-05-15 11:11:31 -0400

    add ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+
+
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
         </header>
     )
 }
@@ -12,4 +21,13 @@ Header.defaultProps = {
     title: 'Task Tracker'
 }
 
+// Header.PropTypes = {
+//     title: propTypes.string.isRequired,
+// }
+
+//css in js
+// const headingStyle = { 
+//     color: 'red', 
+//     backgroundColor: 'black' 
+// }
 export default Header
