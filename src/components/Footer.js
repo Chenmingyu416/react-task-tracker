a559821bd4fee27a8b6030eac13c17b9870e655e
mingyuchen <chen_mingyu416@sina.com>
2022-05-15 12:26:19 -0400

    add json server

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Copyright &copy; 2022
+            </p>
+            <a href='/about'>About</a>
+
+        </footer>
+    )
+}
+
+export default Footer
